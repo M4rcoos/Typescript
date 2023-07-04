84b59785e94b2b693ea57332719d917b0137b8ce
Marcos Vinicius <contato.shopvmax@gmail.com>
2023-07-03 21:32:52 -0300

    first commit

---
+++ Intro/index.ts
@@ -0,0 +1,11 @@
+const firstName = "Marcos";
+const anotherName = 1;
+const x = true;
+
+function greeting(name: string) {
+  console.log("olá " + firstName);
+}
+
+greeting(firstName);
+greeting(anotherName);
+greeting(x);
